fef7b055f6b44f8d80bab527b9d74e6511080eb0
Camilo Suarez <camiloandressuarez1@gmail.com>
2022-11-24 14:11:44 -0500

    :heavy_plus_sign: add dependency and create redux-persist /ts-redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import authReducer from './slices/auth'
+
+const persistAuthConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['accessToken']
+}
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer
-  }
+    auth: persistReducer<ReturnType<typeof authReducer>>(
+      persistAuthConfig,
+      authReducer
+    )
+  },
+  middleware: (defautlMiddleware) =>
+    defautlMiddleware({
+      serializableCheck: false
+    })
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type Dispatch = typeof store.dispatch
+
+export const persistor = persistStore(store)
